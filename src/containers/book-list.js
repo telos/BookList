0f477a04ee56053c22e1d0ac04b70cc39e89a17b
Jacob Parma <jacobparma@gmail.com>
2017-07-05 21:17:06 -0500

    S4-42 Container Implementation

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
-export default class BookList extends Component {
+import { connect } from 'react-redux';
+
+class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
@@ -14,6 +16,15 @@ export default class BookList extends Component {
       <ul className="list-group col-sm-4">
         {this.renderList()}
       </ul>
-    )
+    );
   }
 }
+
+function mapStateToProps(state) {
+  // Whatever is returned will show up as props inside of BookList above
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList)
