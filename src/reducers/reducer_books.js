d5a728318dba60f5d834ad7cfaae1731b5804d4d
Jacob Parma <jacobparma@gmail.com>
2017-07-10 15:56:32 -0500

    S4-49 Wiring and Confitional Rendering

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The Dark Tower' },
-    { title: 'Eloquent Ruby' }
+    { title: 'Javascript: The Good Parts', pages: 50 },
+    { title: 'Harry Potter', pages: 35 },
+    { title: 'The Dark Tower', pages: 75 },
+    { title: 'Eloquent Ruby', pages: 100 }
   ]
 }
